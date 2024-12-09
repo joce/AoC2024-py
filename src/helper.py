3917da1e3e2f7554235add9425ab4b701c353e7a
Jocelyn Legault <jocelynlegault@gmail.com>
2024-12-08 21:56:31 -0500

    Day 6 - Part 1

--- src/helper.py
+++ src/helper.py
@@ -5,3 +5,8 @@ from typing import Callable
 def apply_lines_from_file(filename: str, func: Callable[[list[str]], int]) -> None:
     with open(os.path.join(os.path.dirname(__file__), filename)) as f:
         print(func([line.strip() for line in f.readlines()]))
+
+
+def apply_grid_from_file(filename: str, func: Callable[[list[list[str]]], int]) -> None:
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        print(func([list(line.strip()) for line in f.readlines()]))
