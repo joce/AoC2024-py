f04caa128654397b8df1d2aceb269e8c0dc21af4
Jocelyn Legault <jocelynlegault@gmail.com>
2024-12-26 19:33:33 -0500

    Day 15 - part 1

---
+++ tests/test_day15.py
@@ -0,0 +1,60 @@
+from day15 import BOX, OBSTACLE, ROBOT, parse_input
+
+DATA = """##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
+
+DATA_2 = """########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<"""
+
+
+def test_parse_input():
+    grid, robot = parse_input(DATA)
+    assert grid.w == 10
+    assert grid.h == 10
+    assert grid.start_y == 4
+    assert grid.start_x == 4
+    assert sum(row.count(BOX) for row in grid.grid) == 21
+    assert sum(row.count(OBSTACLE) for row in grid.grid) == 37
+    assert sum(row.count(ROBOT) for row in grid.grid) == 1
+    assert len(robot.directions) == 700
+
+
+def test_move():
+    grid, robot = parse_input(DATA)
+    grid.run_robot(robot)
+    assert robot.x == 3
+    assert robot.y == 4
+
+
+def test_move2():
+    grid, robot = parse_input(DATA_2)
+    grid.run_robot(robot)
+    assert robot.x == 4
+    assert robot.y == 4
